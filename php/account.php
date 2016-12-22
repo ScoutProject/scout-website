95975aefc07192c84ef79ec64b217c1568349ec5
Benjamin Grant <benjamin.grant@wesleycollege.net>
2016-12-22 20:34:17 +1100

    Use api for login
    /v1/login accepts basic auth and returns credentials and user info

--- php/account.php
+++ php/account.php
@@ -1,5 +1,5 @@
 <?php
-function callAPI($method, $url, $data = false) {
+function callAPI($method, $url, $data = false, $user = false, $pass = false) {
     $curl = curl_init();
 
     switch ($method)
@@ -19,8 +19,10 @@ function callAPI($method, $url, $data = false) {
     }
 
     // Optional Authentication:
-    //curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    //curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+	if ($user && $pass) {
+		curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($curl, CURLOPT_USERPWD, "$user:$pass");
+	}
 
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -38,59 +40,29 @@ if (isset($_POST['action'])) {
 	if ($_POST['action'] == "login") {
 		$username = $_POST['user'];
 		$raw_password = $_POST['pass'];
-		if (isset($_POST['remember'])) {
-			$remember = $_POST['remember'];
-		} else {
-			$remember = false;
-		}
 
-		//Connect to the database
-		include_once '../php/database.php';
+		$result = callAPI('GET', 'http://api.scoutdev.ga/v1/login', false, $username, $raw_password);
+		$result = json_decode($result, true);
 
-		$result = mysqli_query($connection, "SELECT id, username, password, email, realName FROM users WHERE username='$username' OR email='$username'");
-		
-		//Query for the username
-		if ($result && mysqli_num_rows($result) > 0) {
-			//Add the results to an array
-			$data = array();
-			$i = 0;
-			while ($row = mysqli_fetch_array($result)) {
-				$data[$i] = array('id' => $row['id'], 'username' => $row['username'], 'password' => $row['password'], 'email' => $row['email'], 'realName' => $row['realName']);
-				$i++;
-			}
-			
-			//If pass correct
-			if (password_verify($raw_password, $data[0]['password'])) {
-				//Password correct!
-				$cookieData = array("username" => $data[0]['username'], "passHash" => $data[0]['password']);
-				setcookie("scouta", base64_encode(json_encode($cookieData)), time()+864000); //Delete after 10 days
-				
-				//Update currentIp
-				$result = mysqli_query($connection, "UPDATE users SET currentIp='" . $_SERVER['REMOTE_ADDR'] . "', lastLoginDate=NOW() WHERE id='" . $data[0]['id'] . "'");
-				
-				//Make a session
-				session_start();
-				$_SESSION['id'] = $data[0]['id'];
-				$_SESSION['user'] = $data[0]['username'];
-				$_SESSION['email'] = $data[0]['email'];
-				$_SESSION['realName'] = $data[0]['realName'];
-				if (isset($_POST['prev_location']) && $_POST['prev_location'] != "/home/") {
-					header('Location: ' . $_POST['prev_location']);
-				} else {
-					header('Location: /');
-				}
-				exit;
+		if (empty($result['status'])) {
+			$cookieData = array("username" => $result['username'], "passHash" => $result['password_hash']);
+			setcookie("scouta", base64_encode(json_encode($cookieData)), time()+864000); //Delete after 10 days
+
+			//Make a session
+			session_start();
+			$_SESSION['id'] = $result['id'];
+			$_SESSION['user'] = $result['username'];
+			$_SESSION['email'] = $result['email'];
+			$_SESSION['realName'] = $result['realName'];
+			if (isset($_POST['prev_location']) && $_POST['prev_location'] != "/home/") {
+				header('Location: ' . $_POST['prev_location']);
 			} else {
-				//Password incorrect
-				$status = "Error: Username or password is incorrect.";
+				header('Location: /');
 			}
+			exit;
 		} else {
-			//Username incorrect
-			$status = "Error: Username or password is incorrect.";
+			$status = $result['msg'];
 		}
-
-		//Close the connection
-		mysqli_close($connection);
 	}
 
 	//Signup
