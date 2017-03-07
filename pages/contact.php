c55c55160ff74807f99872f08abe9db6db1426e8
Benjamin Grant <benjamin.grant@wesleycollege.net>
2017-03-07 15:06:44 +1100

    Finished contact page

--- pages/contact.php
+++ pages/contact.php
@@ -13,7 +13,39 @@ $page = "/contact/";
 		<?php include '../includes/nav.php' ?>
 		<?php include '../includes/modals.php' ?>
 		<main>
-			Contact
+			<div id="contact_container">
+				<h2>Contact</h2>
+				<p>Need to get in touch? Please use the form below. We read everything you send us.</p>
+				<form method="post" action="./">
+					<table cellspacing="0">
+						<tr>
+							<td>
+								<label for="name">Name</label>
+								<input type="text" id="name" name="name" />
+							</td>
+							<td>
+								<label for="email">Email address</label>
+								<input type="email" id="email" name="email" />
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<label for="reason">Reason for contact</label>
+								<select id="reason" name="reason">
+									<option value="comment">Comment</option>
+									<option value="bug">Bug/Issue</option>
+									<option value="suggestion">Suggestion</option>
+									<option value="support">Support</option>
+								</select>
+							</td>
+							<td>
+								<label for="message">Message</label>
+								<textarea id="message" name="message" rows="5"></textarea>
+							</td>
+						</tr>
+					</table>
+				</form>
+			</div>
 		</main>
 		<?php include '../includes/footer.php' ?>
 		<script src="/js/main.js"></script>
